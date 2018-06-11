7698f1a761af33d043bc19b654e8a3efeaefcb20
Magnus Eriksson <magnus.g.eriksson@netinsight.net>
2018-06-11 14:53:43 +0200

    Progress

---
+++ css.js
@@ -0,0 +1,26 @@
+.mountNode {
+  color: #333;
+}
+
+span {
+	display:inline-block;
+  text-align:center;
+	background-color:#ccc;
+  border-radius:50%;
+  margin:0.5em;
+  width:24px;
+}
+
+.fa-star {
+	margin:0.5em;
+  font-size:24px;
+}
+
+.selected {
+  background-color:#ddd;
+  color:lightgray;
+}
+
+.consumed {
+  visibility:hidden;
+}
